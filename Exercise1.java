a978b1aeefca7ce163f722412c1961078c2e9e93
Jagadish1310 <105436692+Jagadish1310@users.noreply.github.com>
2022-07-14 13:48:39 +0530

    Add files via upload

---
+++ Exercise1.java
@@ -0,0 +1,11 @@
+// Write a program to print your Name, Father's Name and Mailing Address in different lines
+
+class Exercise1 {
+ public static void main(String args[])
+  {
+    System.out.println("I am jagadish");
+      System.out.println("Father's name is bimmo behara");
+      System.out.println("email address: jagadishkummari146@gmail.com");
+  }
+
+} 
