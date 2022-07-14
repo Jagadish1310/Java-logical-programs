c43562adc103852214ea6d67645770f1eb630735
Jagadish1310 <105436692+Jagadish1310@users.noreply.github.com>
2022-07-14 14:02:31 +0530

    Add files via upload

---
+++ Exercise1.java
@@ -0,0 +1,12 @@
+// Write a program to initialize a char variable and then store it in an int variable. Print the int variable's value on console.
+
+class Exercise1 {
+	public static void main(String args[]) {
+		char F=20;
+     int D=(int)F;
+    
+    System.out.println(D);
+	
+	}
+
+}
---
+++ Sample1.java
@@ -0,0 +1,18 @@
+// Complete and execute java Program for Narrow/Explicit Type-casting
+
+class Sample1 {
+	public static void main(String args[])
+	{
+		int cs=50;// Initialize a variable to store your marks of Computer Science subject- out of 50
+		
+		float p=75;// Calculate the percentage of marks and store it in a float variable
+		
+		float percentage = cs/p*100;// Cast the percentage to int(whole number, decimal points removed)
+		
+		// Print the casted percentage
+		System.out.println("Percentage of marks in CS = "+percentage );
+    int newpercentage = (int) percentage;
+    System.out.println("Percentage of marks in CS = "+newpercentage );
+	}
+
+}
