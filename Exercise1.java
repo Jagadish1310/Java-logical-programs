ab6d192979332f3d863d4cd9cc8db5060e60230e
Jagadish1310 <105436692+Jagadish1310@users.noreply.github.com>
2022-08-18 18:30:47 +0530

    Add files via upload

---
+++ Exercise1.java
@@ -0,0 +1,32 @@
+//  Write a java method that accepts two numbers as parameters and swap their values
+
+class Exercise1 {
+
+	public static void main(String args[]) {
+
+	main obj=new main();
+    
+    int a=10, b=15;
+   obj.show(a,b);
+    //Call method
+	}
+
+	// Define method
+}
+class main
+  {
+    
+    public void show(int a, int b)
+    {
+      int t;
+      t=a;
+      a=b;
+      b=t;
+     
+     // return a,b;
+      System.out.println(a+" "+b);
+    }
+  }
+	
+
+		    
