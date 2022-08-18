872412ab6ce9e57cbdac4fe70ae61343d4c5db01
Jagadish1310 <105436692+Jagadish1310@users.noreply.github.com>
2022-08-18 18:30:08 +0530

    Add files via upload

---
+++ Exercise1.java
@@ -0,0 +1,22 @@
+//  Write a program to create a Book class and its objects.
+//Create a class which contains the main method 
+class Exercise1 {
+
+	public static void main(String args[]) {
+
+	//Write code here to create two Book objects, assign values to attributes and print
+    Book obj=new Book();
+    obj.price=10;
+    obj.pages=100;
+    System.out.println(obj.price+" "+obj.pages);
+	}
+}
+	
+//Creating Book class
+class Book {  
+    // Declare Book attributes
+  double price;
+  int pages;
+}  
+
+		    
---
+++ Sample1.java
@@ -0,0 +1,27 @@
+//  Complete and execute java program to create Student objects.
+
+//Creating a class which contains the main method 
+class Sample1 {
+
+	public static void main(String args[]) {
+
+	//Write code here to create two Student objects,assign value to their id and name and print the 			//details for both the objects
+    Student obj1=new Student();
+    obj1.id=1;
+    obj1.name="john";
+    Student obj2=new Student();
+    obj2.id=2;
+    obj2.name="smith";
+    System.out.println("1st student details of roll no "+obj1.id+" with name "+obj1.name);
+    System.out.println("2st student details of roll no "+obj2.id+" with name "+obj2.name);
+
+	}
+}
+	
+//Creating Student class
+class Student {  
+int id;  
+String name;  
+}  
+
+		    
